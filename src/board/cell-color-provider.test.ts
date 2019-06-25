50fd9188433cf9d7b9e077cebef1d88e4711bcf9
Oleksii Sukhov <sukhov.oleksii@gmail.com>
2019-06-24 22:49:48 -0500

    implement getCellColor

---
+++ src/board/cell-color-provider.test.ts
@@ -0,0 +1,70 @@
+import { CellColorProvider } from "./cell-color-provider";
+import { Color } from "./color";
+
+const errorMessage = "row and column indexes should be 0 to 7";
+
+test("getCellColor should throw error in case when row index is negative", () => {
+  expect(() => CellColorProvider.getCellColor(-1, 0)).toThrow(errorMessage);
+});
+
+test("getCellColor should throw error in case when row index is more than 7", () => {
+  expect(() => CellColorProvider.getCellColor(8, 0)).toThrow(errorMessage);
+});
+
+test("getCellColor should throw error in case when column index is negative", () => {
+  expect(() => CellColorProvider.getCellColor(0, -1)).toThrow(errorMessage);
+});
+
+test("getCellColor should throw error in case when column index is more than 7", () => {
+  expect(() => CellColorProvider.getCellColor(0, 8)).toThrow(errorMessage);
+});
+
+//   _________________________________
+// 7 |   |BLK|   |BLK|   |BLK|   |BLK|
+//   _________________________________
+// 6 |BLK|   |BLK|   |BLK|   |BLK|   |
+//   _________________________________
+// 5 |   |BLK|   |BLK|   |BLK|   |BLK|
+//   _________________________________
+// 4 |BLK|   |BLK|   |BLK|   |BLK|   |
+//   _________________________________
+// 3 |   |BLK|   |BLK|   |BLK|   |BLK|
+//   _________________________________
+// 2 |BLK|   |BLK|   |BLK|   |BLK|   |
+//   _________________________________
+// 1 |   |BLK|   |BLK|   |BLK|   |BLK|
+//   _________________________________
+// 0 |BLK|   |BLK|   |BLK|   |BLK|   |
+//   _________________________________
+//     0   1   2   3   4   5   6   7
+test("getCellColor should return correct color", () => {
+  const whiteColorCellIndexes = [
+    [0, 1], [0, 3], [0, 5], [0, 7],
+    [1, 0], [1, 2], [1, 4], [1, 6],
+    [2, 1], [2, 3], [2, 5], [2, 7],
+    [3, 0], [3, 2], [3, 4], [3, 6],
+    [4, 1], [4, 3], [4, 5], [4, 7],
+    [5, 0], [5, 2], [5, 4], [5, 6],
+    [6, 1], [6, 3], [6, 5], [6, 7],
+    [7, 0], [7, 2], [7, 4], [7, 6]
+  ];
+
+  const blackColorCellIndexes = [
+    [0, 0], [0, 2], [0, 4], [0, 6],
+    [1, 1], [1, 3], [1, 5], [1, 7],
+    [2, 0], [2, 2], [2, 4], [2, 6],
+    [3, 1], [3, 3], [3, 5], [3, 7],
+    [4, 0], [4, 2], [4, 4], [4, 6],
+    [5, 1], [5, 3], [5, 5], [5, 7],
+    [6, 0], [6, 2], [6, 4], [6, 6],
+    [7, 1], [7, 3], [7, 5], [7, 7]
+  ];
+
+  whiteColorCellIndexes.forEach(cellIndexes => {
+    expect(CellColorProvider.getCellColor(cellIndexes[0], cellIndexes[1])).toBe(Color.White);
+  });
+
+  blackColorCellIndexes.forEach(cellIndexes => {
+    expect(CellColorProvider.getCellColor(cellIndexes[0], cellIndexes[1])).toBe(Color.Black);
+  });
+});
--- src/board/cell-color-provider.ts
+++ src/board/cell-color-provider.ts
@@ -2,6 +2,13 @@ import { Color } from "./color";
 
 export class CellColorProvider {
   public static getCellColor(rowIndex: number, columnIndex: number): Color {
-    throw Error("Not implemented");
+    if (rowIndex < 0 || rowIndex > 7 || columnIndex < 0 || columnIndex > 7) {
+      throw Error("row and column indexes should be 0 to 7");
+    }
+
+    const indexesSum = rowIndex + columnIndex;
+    const isSunEven = indexesSum % 2 === 0;
+
+    return isSunEven ? Color.Black : Color.White;
   }
 }
