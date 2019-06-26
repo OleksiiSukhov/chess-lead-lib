a77a8f53b6f5749bde4063240947c691f99cef25
Oleksii Sukhov <sukhov.oleksii@gmail.com>
2019-06-25 23:07:15 -0500

    add column and row names and ctor for Cell

--- src/board/cell.test.ts
+++ src/board/cell.test.ts
@@ -4,26 +4,23 @@ import { Color } from "../models/color";
 import { Cell } from "./cell";
 import { CellColorProvider } from "./cell-color-provider";
 
-let cell: Cell;
-
-beforeEach(() => {
-  cell = new Cell();
-});
+const errorMessage = "row and column indexes should be 0 to 7";
 
 test("color should return saved color when it was already set to White", () => {
+  const cell = new Cell(0, 0);
   cell["color"] = Color.White;
   expect(cell.cellColor).toBe(Color.White);
 });
 
 test("color should return saved color when it was already set to Black", () => {
+  const cell = new Cell(0, 0);
   cell["color"] = Color.Black;
   expect(cell.cellColor).toBe(Color.Black);
 });
 
 test("color should call CellColorProvider.getCellColor when there is no saved color", () => {
+  const cell = new Cell(1, 2);
   cell["color"] = undefined;
-  cell.rowIndex = 1;
-  cell.columnIndex = 2;
 
   const getCellColorMock = jest.fn();
   getCellColorMock.mockReturnValue(Color.Black);
@@ -32,4 +29,36 @@ test("color should call CellColorProvider.getCellColor when there is no saved co
 
   expect(cell.cellColor).toBe(Color.Black);
   expect(getCellColorMock).toHaveBeenCalledWith(1, 2);
+});
+
+test("rowName should return correct name of row (1-8)", () => {
+  [...Array(8).keys()].forEach(index => {
+    const cell = new Cell(index, 0);
+    expect(cell.rowName).toBe((index + 1).toString());
+  });
+});
+
+test("columnName should return correct name of column (a-h)", () => {
+  const map: Array<[number, string]> = [[0, "a"], [0, "a"], [0, "a"], [0, "a"], [0, "a"], [0, "a"], [0, "a"], [0, "a"]];
+
+  map.forEach(indexToName => {
+    const cell = new Cell(0, indexToName[0]);
+    expect(cell.columnName).toBe(indexToName[1]);
+  });
+});
+
+test("constructor should throw error in case when row index is negative", () => {
+  expect(() => new Cell(-1, 0)).toThrow(errorMessage);
+});
+
+test("constructor should throw error in case when row index is more than 7", () => {
+  expect(() => new Cell(8, 0)).toThrow(errorMessage);
+});
+
+test("constructor should throw error in case when column index is negative", () => {
+  expect(() => new Cell(0, -1)).toThrow(errorMessage);
+});
+
+test("constructor should throw error in case when column index is more than 7", () => {
+  expect(() => new Cell(0, 8)).toThrow(errorMessage);
 });
--- src/board/cell.ts
+++ src/board/cell.ts
@@ -2,15 +2,34 @@ import { Color } from "../models/color";
 import { CellColorProvider } from "./cell-color-provider";
 
 export class Cell {
-  public rowIndex: number = 0;
-  public columnIndex: number = 0;
-  private color?: Color;
+  public readonly rowIndex: number = 0;
+  public readonly columnIndex: number = 0;
 
+  public get rowName(): string {
+    return (this.rowIndex + 1).toString();
+  }
+
+  public get columnName(): string {
+    const aAsciiCode = 97;
+
+    return String.fromCharCode(aAsciiCode + this.columnIndex);
+  }
+
+  private color?: Color;
   public get cellColor(): Color {
     if (this.color) {
       return this.color;
     }
 
     return CellColorProvider.getCellColor(this.rowIndex, this.columnIndex);
+  }
+
+  constructor(rowIndex: number, columnIndex: number) {
+    if (rowIndex < 0 || rowIndex > 7 || columnIndex < 0 || columnIndex > 7) {
+      throw Error("row and column indexes should be 0 to 7");
+    }
+
+    this.rowIndex = rowIndex;
+    this.columnIndex = columnIndex;
   }
 }
