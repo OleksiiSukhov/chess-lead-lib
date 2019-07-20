842955605565074a237f151d45566e1c0404ed81
Oleksii Sukhov <sukhov.oleksii@gmail.com>
2019-07-20 13:48:47 -0500

    clean up bishop tests

--- src/board/movements/bishop-movements.test.ts
+++ src/board/movements/bishop-movements.test.ts
@@ -2,11 +2,19 @@ import { isEqual, xorWith } from "lodash";
 
 import { Bishop } from "../../chess-pieces/bishop";
 import { King } from "../../chess-pieces/king";
+import { Queen } from "../../chess-pieces/queen";
 import { Color } from "../../models/color";
 import { TestBoardAssistance } from "../../tests/test-board-assistance";
 import { Cell } from "../cell";
 import { BishopMovements } from "./bishop-movements";
-import { Queen } from "../../chess-pieces/queen";
+
+let bishopMovements: BishopMovements;
+let boardCells: Cell[][];
+
+beforeEach(() => {
+  bishopMovements = new BishopMovements();
+  boardCells = TestBoardAssistance.setupEmptyBoard();
+});
 
 //   _________________________________
 // 7 |   |   |   |   |BKI|   |   | + |
@@ -27,16 +35,13 @@ import { Queen } from "../../chess-pieces/queen";
 //   _________________________________
 //     0   1   2   3   4   5   6   7
 test("getAvailable should return correct cells for empty board", () => {
-  const bishopMovements = new BishopMovements();
-
-  const boardCells: Cell[][] = TestBoardAssistance.setupEmptyBoard();
   const currentCell = { rowIndex: 3, columnIndex: 3, chessPiece: new Bishop(Color.White) } as Cell;
 
   boardCells[3][3] = currentCell;
   boardCells[7][4] = { rowIndex: 7, columnIndex: 4, chessPiece: new King(Color.Black) } as Cell;
   boardCells[0][4] = { rowIndex: 0, columnIndex: 4, chessPiece: new King(Color.White) } as Cell;
 
-  const expectedAvailableCells: Cell[] = [
+  const expected: Cell[] = [
     boardCells[6][0],
     boardCells[5][1],
     boardCells[4][2],
@@ -52,9 +57,7 @@ test("getAvailable should return correct cells for empty board", () => {
     boardCells[0][6],
   ];
 
-  const actual = bishopMovements.getAvailable(boardCells, currentCell);
-
-  expect(xorWith(actual, expectedAvailableCells, isEqual).length).toBe(0);
+  assertAvailableMovementCells(expected, currentCell);
 });
 
 //   _________________________________
@@ -76,9 +79,6 @@ test("getAvailable should return correct cells for empty board", () => {
 //   _________________________________
 //     0   1   2   3   4   5   6   7
 test("getAvailable should return correct cells for board with enemy on the way", () => {
-  const bishopMovements = new BishopMovements();
-
-  const boardCells: Cell[][] = TestBoardAssistance.setupEmptyBoard();
   const currentCell = { rowIndex: 3, columnIndex: 3, chessPiece: new Bishop(Color.White) } as Cell;
 
   boardCells[3][3] = currentCell;
@@ -86,7 +86,7 @@ test("getAvailable should return correct cells for board with enemy on the way",
   boardCells[0][4] = { rowIndex: 0, columnIndex: 4, chessPiece: new King(Color.White) } as Cell;
   boardCells[5][1] = { rowIndex: 5, columnIndex: 1, chessPiece: new Queen(Color.Black) } as Cell;
 
-  const expectedAvailableCells: Cell[] = [
+  const expected: Cell[] = [
     boardCells[5][1],
     boardCells[4][2],
     boardCells[0][0],
@@ -101,7 +101,11 @@ test("getAvailable should return correct cells for board with enemy on the way",
     boardCells[0][6],
   ];
 
+  assertAvailableMovementCells(expected, currentCell);
+});
+
+function assertAvailableMovementCells(expected: Cell[], currentCell: Cell): void {
   const actual = bishopMovements.getAvailable(boardCells, currentCell);
 
-  expect(xorWith(actual, expectedAvailableCells, isEqual).length).toBe(0);
-});
+  expect(xorWith(actual, expected, isEqual).length).toBe(0);
+}
