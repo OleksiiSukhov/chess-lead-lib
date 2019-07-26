8f2fbf52fdf1d68989d696d5fd11f81f6dd3006d
Oleksii Sukhov <sukhov.oleksii@gmail.com>
2019-07-25 22:06:21 -0500

    more tests for pawn when no any options to move

--- src/board/movements/pawn-movements.test.ts
+++ src/board/movements/pawn-movements.test.ts
@@ -266,8 +266,68 @@ test("getAvailable should return correct cells for initial position and diagonal
   assertAvailableMovementCells(expected, currentCell);
 });
 
-// todo: no any options to move (white)
-// todo: no any options to move (black)
+//   _________________________________
+// 7 |   |   |   |   |BKI|   |   |   |
+//   _________________________________
+// 6 |   |   |   |   |   |   |   |   |
+//   _________________________________
+// 5 |   |   |   |   |   |   |   |   |
+//   _________________________________
+// 4 |   |   |   |   |   |   |   |   |
+//   _________________________________
+// 3 |   |   |   |WQ |   |   |   |   |
+//   _________________________________
+// 2 |   |   |   |WP |   |   |   |   |
+//   _________________________________
+// 1 |   |   |   |   |   |   |   |   |
+//   _________________________________
+// 0 |   |   |   |   |WKI|   |   |   |
+//   _________________________________
+//     0   1   2   3   4   5   6   7
+test("getAvailable should return empty array when no any options (white)", () => {
+  const currentCell = { rowIndex: 2, columnIndex: 3, chessPiece: new Pawn(Color.White) } as Cell;
+
+  boardCells[2][3] = currentCell;
+  boardCells[7][4] = { rowIndex: 7, columnIndex: 4, chessPiece: new King(Color.Black) } as Cell;
+  boardCells[0][4] = { rowIndex: 0, columnIndex: 4, chessPiece: new King(Color.White) } as Cell;
+  boardCells[3][3] = { rowIndex: 3, columnIndex: 3, chessPiece: new Queen(Color.White) } as Cell;
+
+  const expected: Cell[] = [];
+
+  assertAvailableMovementCells(expected, currentCell);
+});
+
+//   _________________________________
+// 7 |   |   |   |   |BKI|   |   |   |
+//   _________________________________
+// 6 |   |   |   |   |   |   |   |   |
+//   _________________________________
+// 5 |   |   |   |BP |   |   |   |   |
+//   _________________________________
+// 4 |   |   |   |BQ |   |   |   |   |
+//   _________________________________
+// 3 |   |   |   |   |   |   |   |   |
+//   _________________________________
+// 2 |   |   |   |   |   |   |   |   |
+//   _________________________________
+// 1 |   |   |   |   |   |   |   |   |
+//   _________________________________
+// 0 |   |   |   |   |WKI|   |   |   |
+//   _________________________________
+//     0   1   2   3   4   5   6   7
+test("getAvailable should return empty array when no any options (black)", () => {
+  const currentCell = { rowIndex: 5, columnIndex: 3, chessPiece: new Pawn(Color.Black) } as Cell;
+
+  boardCells[5][3] = currentCell;
+  boardCells[7][4] = { rowIndex: 7, columnIndex: 4, chessPiece: new King(Color.Black) } as Cell;
+  boardCells[0][4] = { rowIndex: 0, columnIndex: 4, chessPiece: new King(Color.White) } as Cell;
+  boardCells[4][3] = { rowIndex: 4, columnIndex: 3, chessPiece: new Queen(Color.Black) } as Cell;
+
+  const expected: Cell[] = [];
+
+  assertAvailableMovementCells(expected, currentCell);
+});
+
 // todo: promotion (white)
 // todo: promotion (black)
 // todo: en passant (white)
