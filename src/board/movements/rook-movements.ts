a44331cac47d7350758ec099fa24424f722c64e0
Oleksii Sukhov <sukhov.oleksii@gmail.com>
2019-08-03 15:08:05 -0500

    clean up getAdjustedAvailableCellsWithCheckChecking

--- src/board/movements/rook-movements.ts
+++ src/board/movements/rook-movements.ts
@@ -1,6 +1,8 @@
 import { cloneDeep } from "lodash";
+import { ChessPiece } from "../../chess-pieces/chess-piece";
 import { King } from "../../chess-pieces/king";
 import { BoardState } from "../../models/board-state";
+import { Color } from "../../models/color";
 import { Cell } from "../cell";
 import { Movements } from "./movements";
 
@@ -24,100 +26,117 @@ export class RookMovements extends Movements {
   ): Cell[] {
     this.validateGetAvailableArguments(boardState.board, currentCell);
 
-    let availableSquares = this.getAvailableBasedOnDirections(boardState.board, currentCell);
+    const availableSquares = this.getAvailableBasedOnDirections(boardState.board, currentCell);
 
     if (!checkCheckingNeeded) {
       return availableSquares;
     }
 
-    const initialSquareState = cloneDeep(
-      boardState.board[currentCell.rowIndex][currentCell.columnIndex],
+    return this.getAdjustedAvailableCellsWithCheckChecking(
+      availableSquares,
+      boardState,
+      currentCell,
     );
+  }
 
-    if (!initialSquareState.chessPiece) {
-      throw Error("chess piece for current square should be defined");
+  public getAdjustedAvailableCellsWithCheckChecking(
+    initialAvailableCells: Cell[],
+    boardState: BoardState,
+    currentCell: Cell,
+  ): Cell[] {
+    const initialChessPiece = cloneDeep(currentCell.chessPiece);
+
+    if (!initialChessPiece) {
+      throw Error("Current cell chess piece should be defined.");
     }
 
-    const currentChessPieceColor = initialSquareState.chessPiece.color;
+    const allyKingCell = this.getAllyKingCell(boardState.board, initialChessPiece.color);
+    const boardStateCopy = cloneDeep(boardState);
 
-    let allyKingSquare: Cell | undefined;
+    boardStateCopy.board[currentCell.rowIndex][currentCell.columnIndex].chessPiece = undefined;
 
+    initialAvailableCells.forEach(availableCell => {
+      const previousCell = cloneDeep(
+        boardStateCopy.board[availableCell.rowIndex][availableCell.columnIndex],
+      );
+
+      this.makeTestMovement(boardStateCopy.board, availableCell, initialChessPiece);
+
+      initialAvailableCells = this.getAdjustedCells(
+        initialAvailableCells,
+        boardStateCopy,
+        allyKingCell,
+        availableCell,
+      );
+
+      boardStateCopy.board[availableCell.rowIndex][availableCell.columnIndex] = previousCell;
+    });
+
+    return initialAvailableCells;
+  }
+
+  private makeTestMovement(board: Cell[][], cellToMove: Cell, chessPiece: ChessPiece): void {
+    board[cellToMove.rowIndex][cellToMove.columnIndex].chessPiece = cloneDeep(chessPiece);
+  }
+
+  private getAllyKingCell(board: Cell[][], currentColor: Color): Cell {
     for (let row = 0; row < 8; row++) {
-      if (allyKingSquare) {
-        break;
-      }
-
       for (let cell = 0; cell < 8; cell++) {
-        const chessPiece = boardState.board[row][cell].chessPiece;
+        const chessPiece = board[row][cell].chessPiece;
 
-        if (
-          chessPiece &&
-          chessPiece instanceof King &&
-          chessPiece.color === currentChessPieceColor
-        ) {
-          allyKingSquare = boardState.board[row][cell];
-          break;
+        if (chessPiece && chessPiece instanceof King && chessPiece.color === currentColor) {
+          return board[row][cell];
         }
       }
     }
 
-    const boardStateCopy = cloneDeep(boardState);
+    throw Error("Ally King was not found.");
+  }
 
-    // remove current chess piece from current cell
-    boardStateCopy.board[currentCell.rowIndex][currentCell.columnIndex].chessPiece = undefined;
-
-    // make a test move (using initial list of available cells)
-    availableSquares.forEach(availableSquare => {
-      const previousSquareState = cloneDeep(
-        boardStateCopy.board[availableSquare.rowIndex][availableSquare.columnIndex],
-      );
-
-      // simulate move of current cess piece to next available cell
-      boardStateCopy.board[availableSquare.rowIndex][
-        availableSquare.columnIndex
-      ].chessPiece = cloneDeep(initialSquareState.chessPiece);
-
-      // get all enemies available cells
-      boardStateCopy.board.forEach(boardRow => {
-        boardRow.forEach(boardCell => {
-          const chessPiece = boardCell.chessPiece;
-          if (!chessPiece || chessPiece.color === currentChessPieceColor) {
-            return;
-          }
-
-          // get available cells for current chess piece
-          const enemyAvailableCells = chessPiece
-            .movements()
-            .getAvailable(boardStateCopy, boardCell, false);
-
-          // check if current color king is in check for current enemyAvailableCells
-          const isKingInCheck = enemyAvailableCells.some(enemyAvailableCell => {
-            if (!allyKingSquare) {
-              return false;
-            }
-
-            return (
-              enemyAvailableCell.rowIndex === allyKingSquare.rowIndex &&
-              enemyAvailableCell.columnIndex === allyKingSquare.columnIndex
-            );
-          });
-
-          if (isKingInCheck) {
-            availableSquares = availableSquares.filter(
-              square =>
-                square.rowIndex !== availableSquare.rowIndex ||
-                square.columnIndex !== availableSquare.columnIndex,
-            );
-          }
-        });
+  private getAdjustedCells(
+    initialAvailableCells: Cell[],
+    boardState: BoardState,
+    allyKingCell: Cell,
+    availableSquare: Cell,
+  ): Cell[] {
+    boardState.board.forEach(boardRow => {
+      boardRow.forEach(boardCell => {
+        if (this.isKingInCheck(boardCell, boardState, allyKingCell)) {
+          initialAvailableCells = initialAvailableCells.filter(
+            square =>
+              square.rowIndex !== availableSquare.rowIndex ||
+              square.columnIndex !== availableSquare.columnIndex,
+          );
+        }
       });
-
-      // restore previousSquareState and initial current cell on the board
-      boardStateCopy.board[availableSquare.rowIndex][
-        availableSquare.columnIndex
-      ] = previousSquareState;
     });
 
-    return availableSquares;
+    return initialAvailableCells;
+  }
+
+  private isKingInCheck(
+    boardCellToCheck: Cell,
+    boardState: BoardState,
+    allyKingCell: Cell,
+  ): boolean {
+    const chessPiece = boardCellToCheck.chessPiece;
+
+    if (
+      !chessPiece ||
+      !allyKingCell.chessPiece ||
+      chessPiece.color === allyKingCell.chessPiece.color
+    ) {
+      return false;
+    }
+
+    const currentEnemyAvailableCells = chessPiece
+      .movements()
+      .getAvailable(boardState, boardCellToCheck, false);
+
+    return currentEnemyAvailableCells.some(
+      enemyAvailableCell =>
+        enemyAvailableCell.rowIndex === allyKingCell.rowIndex &&
+        enemyAvailableCell.columnIndex === allyKingCell.columnIndex,
+    );
   }
 }
