d7e3467b538f05654849ad2ca50ceeaee8f302ce
Oleksii Sukhov <sukhov.oleksii@gmail.com>
2019-09-07 16:08:06 -0500

    Remove redundant DrawOnTime type

--- src/models/draw-type.ts
+++ src/models/draw-type.ts
@@ -3,6 +3,5 @@ export enum DrawType {
   Stalemate = "Stalemate",
   ThreefoldRepetition = "ThreefoldRepetition", // todo: to be implemented
   FiftyMoveRule = "FiftyMoveRule",  // todo: to be implemented
-  InsufficientMaterial = "InsufficientMaterial",
-  DrawOnTime = "DrawOnTime",  // todo: to be implemented
+  InsufficientMaterial = "InsufficientMaterial"
 }
